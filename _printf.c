66abc6dc5c4ae4c6dc037615aba0d17ae4ddd9ee
Sipho <mabirimisesipho@gmail.com>
2023-03-26 10:26:51 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -1,46 +1,56 @@
 #include <stdio.h>
 #include <stdarg.h>
 /**
- * printf - main function
+ * _printf - main function
  * @format: pointer parameter used
  * Return: count
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
+
 	va_start(args, format);
 
 	int count = 0;
-	while (*format != '\0') {
-	if (*format == '%') {
-	format++;
-	switch (*format) {
-	case 'c': {
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+	{
+		format++;
+	switch (*format)
+	{
+	case 'c':
+	{
 	char c = va_arg(args, int);
+
 	putchar(c);
 	count++;
 	break;
 	}
-	case 's': {
+	case 's':
+	{
 	char *s = va_arg(args, char *);
+
 	fputs(s, stdout);
 	count += strlen(s);
 	break;
 	}
-	case '%': {
+	case '%':
+	{
 	putchar('%');
 	count++;
 	break;
-		  }
-            }
-        }
-        else {
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
-    
-    va_end(args);
+	}
+	}
+	}
+	else
+	{
+	putchar(*format);
+	count++;
+	}
+	format++;
+	}
+	va_end(args);
 	return (count);
 }
