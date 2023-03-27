9a70b0b1e58cd444eeb5747e78e276c35929c464
Sipho <mabirimisesipho@gmail.com>
2023-03-27 08:59:04 -0700

    handlers

--- write_handlers.c
+++ write_handlers.c
@@ -149,52 +149,35 @@ int write_num(int ind, char buffer[],
  *
  * Return: Number of written chars.
  */
-int write_unsgnd(int is_negative, int ind,
-	char buffer[],
+int handle_write_float(double value, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	/* The number is stored at the bufer's right and starts at position i */
-	int length = BUFF_SIZE - ind - 1, i = 0;
+	int i = 0;
 	char padd = ' ';
 
-	UNUSED(is_negative);
-	UNUSED(size);
-
-	if (precision == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
-		return (0); /* printf(".0d", 0)  no char is printed */
-
-	if (precision > 0 && precision < length)
-		padd = ' ';
-
-	while (precision > length)
-	{
-		buffer[--ind] = '0';
-		length++;
-	}
-
-	if ((flags & F_ZERO) && !(flags & F_MINUS))
+	if (flags & F_ZERO)
 		padd = '0';
+	i = snprintf(buffer, BUFF_SIZE, "%.*f", precision, value);
 
-	if (width > length)
+	if (i < 0 || i >= BUFF_SIZE)
+	return (-1)
+
+	if (width > i)
 	{
-		for (i = 0; i < width - length; i++)
-			buffer[i] = padd;
+	buffer[BUFF_SIZE - 1] = '\0';
+	for (int j = 0; j < width - i; j++)
+	buffer[BUFF_SIZE - j - 2] = padd;
 
-		buffer[i] = '\0';
-
-		if (flags & F_MINUS) /* Asign extra char to left of buffer [buffer>padd]*/
-		{
-			return (write(1, &buffer[ind], length) + write(1, &buffer[0], i));
-		}
-		else /* Asign extra char to left of padding [padd>buffer]*/
-		{
-			return (write(1, &buffer[0], i) + write(1, &buffer[ind], length));
-		}
+	if (flags & F_MINUS)
+	return (write(1, &buffer[0], i) +
+	write(1, &buffer[BUFF_SIZE - j - 1], width - i));
+	else
+	return (write(1, &buffer[BUFF_SIZE - j - 1], width - i) +
+	write(1, &buffer[0], i));
 	}
 
-	return (write(1, &buffer[ind], length));
+	return (write(1, &buffer[0], i));
 }
-
 /**
  * write_pointer - Write a memory address
  * @buffer: Arrays of chars
